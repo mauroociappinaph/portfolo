8c0995c70daa4ef375b0fb0431cd608dfed05082
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-07-15 11:25:34 -0300

    Form & Skills

--- portfolio/src/Components/Form/Form.jsx
+++ portfolio/src/Components/Form/Form.jsx
@@ -1,51 +1,65 @@
-import React from 'react';
-import { useFormik } from 'formik';
+import React, { useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
-import styles from './Form.module.css'; // Reemplaza "Form" con el nombre de tu componente
 
 const Form = () => {
-  // Resto del código del componente...
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
 
   return (
     <>
       <NavBar />
-      <div className={styles.container}>
-        <h1 className={styles.title}>Contact Form</h1>
-        <form onSubmit={formik.handleSubmit}>
-          <div className={styles.formGroup}>
-            <label htmlFor="name">Name:</label>
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold mb-4">Contact Form</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-sm font-bold mb-2">Name:</label>
             <input
               type="text"
               id="name"
               name="name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              className={styles.input}
+              value={formState.name}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="email">Email:</label>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-bold mb-2">Email:</label>
             <input
               type="email"
               id="email"
               name="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              className={styles.input}
+              value={formState.email}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="message">Message:</label>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-sm font-bold mb-2">Message:</label>
             <textarea
               id="message"
               name="message"
-              value={formik.values.message}
-              onChange={formik.handleChange}
-              className={styles.textarea}
+              value={formState.message}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
-          <button type="submit" className={styles.button}>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
             Submit
           </button>
         </form>
--- portfolio/src/Components/Skills/Skills.jsx
+++ portfolio/src/Components/Skills/Skills.jsx
@@ -1,18 +1,56 @@
-import React from "react";
-import NavBar from "../NavBar/NavBar";
-import Footer from "../Footer/Footer";
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
 
-const Skills = () => {
-  return (
-    <>
-      <NavBar />
-      <div>
-        <h1>Skills</h1>
-        {/* Agrega aquí el contenido de tu página de habilidades */}
-      </div>
-      <Footer />
-    </>
-  );
-};
+const skills = [
+  {
+    name: 'JavaScript',
+    description: 'Un lenguaje de programación interpretado que es fundamental para el desarrollo web.',
+    icon_path: '/path/to/javascript_icon.png',
+  },
+  {
+    name: 'Next.js',
+    description: 'Un marco de trabajo basado en React que permite la generación de sitios estáticos y aplicaciones web renderizadas en el servidor.',
+    icon_path: '/path/to/nextjs_icon.png',
+  },
+  {
+    name: 'Node.js',
+    description: 'Una plataforma en tiempo de ejecución de JavaScript, basada en el motor V8 de Chrome, que permite ejecutar código JavaScript en el servidor.',
+    icon_path: '/path/to/nodejs_icon.png',
+  },
+  {
+    name: 'React',
+    description: 'Una biblioteca de JavaScript para construir interfaces de usuario, generalmente para aplicaciones de una sola página.',
+    icon_path: '/path/to/react_icon.png',
+  },
+  {
+    name: 'Redux',
+    description: 'Una biblioteca de JavaScript para administrar el estado de la aplicación. Es más comúnmente usado con bibliotecas como React o Angular para construir interfaces de usuario.',
+    icon_path: '/path/to/redux_icon.png',
+  },
+];
+
+const Card = ({ skill }) => (
+  <div className="rounded overflow-hidden shadow-lg m-4">
+    <img className="w-full h-32 object-cover" src={skill.icon_path} alt={`${skill.name} Icon`} />
+    <div className="px-6 py-4">
+      <div className="font-bold text-xl mb-2">{skill.name}</div>
+      <p className="text-gray-700 text-base">{skill.description}</p>
+    </div>
+  </div>
+);
+
+const Skills = () => (
+  <>
+    <NavBar />
+    <div className="container mx-auto px-4 grid grid-cols-5 gap-4">
+      <h1 className="text-3xl font-bold mb-4 w-full text-center">Mis habilidades</h1>
+      {skills.map((skill) => (
+        <Card skill={skill} key={skill.name} />
+      ))}
+    </div>
+    <Footer />
+  </>
+);
 
 export default Skills;
